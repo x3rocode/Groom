5086ae46f10a1f225e7e60be686f72e4869e8bbb
fascinate98 <fascinate982@naver.com>
2022-10-17 16:46:49 +0900

    nononono...

--- app/src/main/java/com/fascinate98/groom/presentation/MainActivity.kt
+++ app/src/main/java/com/fascinate98/groom/presentation/MainActivity.kt
@@ -17,8 +17,22 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            GroomTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("ggg")
+                }
+            }
+        }
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
